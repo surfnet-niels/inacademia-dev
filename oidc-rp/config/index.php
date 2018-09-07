9911bb6edf034dc6b470207959ebf389ad91cd91
Niels van Dijk <niels.vandijk@surfnet.nl>
2018-09-07 14:31:07 +0200

    Updated index.php to only show OIDC related info

--- oidc-rp/config/index.php
+++ oidc-rp/config/index.php
@@ -1,1 +1,9 @@
-<?php phpinfo(); ?>
+<?php
+foreach (getallheaders() as $name => $value) {
+   $OIDC_header = strpos($name , "OIDC_CLAIM");
+   if($OIDC_header !== false) {
+        echo "<b>$name</b>: $value</br>";
+   }
+}
+?>
+
