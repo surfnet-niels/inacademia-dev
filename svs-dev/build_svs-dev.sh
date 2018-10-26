5ae0302d5cb8d63ed75dc73d3781ec33c705965c
Martin van Es <martin@mrvanes.com>
2018-10-26 17:24:47 +0200

    Build now creates, run only starts container

--- svs-dev/build_svs-dev.sh
+++ svs-dev/build_svs-dev.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-
-IMAGE_TAG=inacademia/svs-dev:v1
+source svs-dev.cnf
 
 # make sure we have the latest metadata from the IdP
 /usr/bin/curl -ksS https://idp.inacademia.local/saml2/idp/metadata.php > config/production/idp.xml
@@ -19,3 +18,33 @@ echo "Building  docker container $IMAGE_TAG ..."
 # Build the docker image
 docker build -t $IMAGE_TAG .
 
+# find the location of configs in current directory structure
+RUN_DIR=$PWD
+CONFIG_DIR="$RUN_DIR/config"
+
+# Remove existing container
+docker rm $CONTAINER_NAME
+
+# Create SVS
+docker create -it \
+    --name $CONTAINER_NAME \
+    --env PROXY_PORT=80 \
+    --env SATOSA_STATE_ENCRYPTION_KEY=1fa0dafd36d9d2c8401b943sk4kwde954e2016cf3f37fa1f67bbffe6c4f2f78e \
+    --env SATOSA_USER_ID_HASH_SALT=6f692915a7df20d9d4be17a70djdieff04585b0ab231825b7a15ed5d6140aa1e \
+    -v $CONFIG_DIR/production:/var/svs \
+    -v $CONFIG_DIR/cdb:/etc/cdb \
+    -v /etc/passwd:/etc/passwd:ro   \
+    -v /etc/group:/etc/group:ro \
+    -v $PWD/workdir:/opt/workdir \
+    -e DATA_DIR=/var/svs \
+    -w /var/svs \
+    --net inacademia.local \
+    --ip 172.172.172.1 \
+    --add-host=svs.inacademia.local:172.172.172.1 \
+    --add-host=op.inacademia.local:172.172.172.2 \
+    --add-host=rp.inacademia.local:172.172.172.100 \
+    --add-host=idp.inacademia.local:172.172.172.200 \
+    --hostname svs.inacademia.local \
+    --expose 80 \
+    --expose 443 \
+    $IMAGE_TAG
