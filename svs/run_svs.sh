7f02dd612e7f69a38684886a1c1b40f893d94bd6
Martin van Es <martin@mrvanes.com>
2018-11-02 13:56:03 +0100

    Reinstate IdP metadata refresh before run

--- svs/run_svs.sh
+++ svs/run_svs.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 source svs.cnf
 
+# Get fresh idp metadata
+curl https://idp.inacademia.local/saml2/idp/metadata.php -o config/production/idp.xml -k
+
 # Start SVS
 docker start -i $CONTAINER_NAME
