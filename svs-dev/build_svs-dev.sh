820209c232c4dd9a7f6650fcd2738c158289a64f
Niels van Dijk <niels.vandijk@surfnet.nl>
2019-04-30 10:10:26 +0200

    Updated svs-dev to improve handlign of remote repositories

--- svs-dev/build_svs-dev.sh
+++ svs-dev/build_svs-dev.sh
@@ -2,10 +2,15 @@
 source svs-dev.cnf
 
 # make sure we have the latest metadata from the IdP
-/usr/bin/curl -ksS https://idp.inacademia.local/saml2/idp/metadata.php > config/production/idp.xml
+/usr/bin/curl -ksS https://idp.inacademia.local/saml2/idp/metadata.php > config/production/idp.xml || exit 3
+
+# make sure we have the ssh key we can use to pull and push from git
+/bin/cp $GIT_KEY_PATH config/id_rsa_inacademia || exit 3
+
+MY_HOST_UID=$(id -g)
 
 # create workdir
-mkdir -p workdir
+mkdir -p workdir || exit 3
 
 # As the build command is being called, we assume we need to build a new image.
 # To be sure we therefor first remove existign ones
@@ -28,6 +33,9 @@ docker rm $CONTAINER_NAME
 # Create SVS
 docker create -it \
     --name $CONTAINER_NAME \
+    --env SATOSA_TAG=$SATOSA_TAG \
+    --env SVS_TAG=$SVS_TAG \
+    --env MY_HOST_UID=$MY_HOST_UID \
     --env PROXY_PORT=80 \
     --env SATOSA_STATE_ENCRYPTION_KEY=1fa0dafd36d9d2c8401b943sk4kwde954e2016cf3f37fa1f67bbffe6c4f2f78e \
     --env SATOSA_USER_ID_HASH_SALT=6f692915a7df20d9d4be17a70djdieff04585b0ab231825b7a15ed5d6140aa1e \
@@ -48,3 +56,6 @@ docker create -it \
     --expose 80 \
     --expose 443 \
     $IMAGE_TAG
+
+# Remove the key so it does not get into git by accident
+rm config/id_rsa_inacademia
--- svs-dev/config/install.sh
+++ svs-dev/config/install.sh
@@ -3,12 +3,18 @@ if ! [ -f /.dockerenv ]; then
     echo "I'm not a docker";
     exit 1
 fi
+
+echo "Using SaToSa version:" $SATOSA_TAG
+echo "Using SVS version:" $SVS_TAG
+GIT_SSH_COMMAND='ssh -i /tmp/inacademia/id_rsa_inacademia -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
 mkdir -p /opt/workdir
-git clone -b svs-1.0.1 https://github.com/InAcademia/SATOSA.git /opt/workdir/satosa || git -C /opt/workdir/satosa pull
-git clone https://github.com/inacademia-development/svs.git /opt/workdir/svs || git -C /opt/workdir/svs pull
+/usr/bin/git clone -b $SATOSA_TAG git@github.com:inacademia-development/SATOSA.git /opt/workdir/satosa || /usr/bin/git -C /opt/workdir/satosa checkout -b $SATOSA_TAG  && /usr/bin/git -C /opt/workdir/satosa pull origin $SATOSA_TAG
+/usr/bin/git clone -b $SVS_TAG git@github.com:inacademia-development/svs.git /opt/workdir/svs || /usr/bin/git -C /opt/workdir/svs checkout -b $SVS_TAG && /usr/bin/git -C /opt/workdir/svs pull origin $SVS_TAG
 virtualenv --python=python3 /opt/workdir/svs_venv
 source /opt/workdir/svs_venv/bin/activate
-pip3 install -e -U /opt/workdir/satosa
-pip3 install -e -U /opt/workdir/svs
+pip3 install -e /opt/workdir/satosa
+pip3 install -e /opt/workdir/svs
 # pip3 install pystache
 touch /opt/workdir/svs_venv/installed
+/bin/chown -Rf $MY_HOST_UID /opt/workdir
